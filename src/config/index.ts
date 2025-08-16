a0ac2113b560a90312535b43d0efb47c9162b349
Anunay Argha <anunayargha@gmail.com>
2025-08-16 10:47:53 +0600

    Update the user model

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,11 +3,19 @@ import "dotenv/config";
 interface Config {
 	port: number | string;
 	mongoURI: string;
+	jwtAccessTokenSecret: string;
+	jwtAccessTokenExpiry: string;
+	jwtRefreshTokenSecret: string;
+	jwtRefreshTokenExpiry: string;
 }
 
 const config: Config = {
 	port: process.env.PORT || 3000,
 	mongoURI: process.env.MONGO_URI!,
+	jwtAccessTokenSecret: process.env.JWT_ACCESS_TOKEN_EXPIRY!,
+	jwtAccessTokenExpiry: process.env.JWT_ACCESS_TOKEN_EXPIRY!,
+	jwtRefreshTokenSecret: process.env.JWT_REFRESH_TOKEN_SECRET!,
+	jwtRefreshTokenExpiry: process.env.JWT_REFRESH_TOKEN_EXPIRY!,
 };
 
 export default config;
--- src/models/user.ts
+++ src/models/user.ts
@@ -1,5 +1,7 @@
 import { model, Schema, Document, Model } from "mongoose";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import config from "../config/index.ts";
 
 export interface IAddress {
 	street: string;
@@ -12,25 +14,29 @@ export interface IAddress {
 export interface IUser extends Document {
 	name: string;
 	email: string;
+	phone: string;
 	photo: string;
 	role: "user" | "admin";
-	password: string;
-	shippingAddress: IAddress;
-	billingAddress: IAddress;
-	passwordChangedAt: Date;
-	passwordResetToken: string;
-	passwordResetExpire: Date;
+	password?: string;
+	shippingAddress?: IAddress;
+	billingAddress?: IAddress;
+	passwordChangedAt?: Date;
+	passwordResetToken?: string;
+	passwordResetExpire?: Date;
 	refreshTokens: string[];
 	mfaEnabled: boolean;
-	mfaSecret: string;
+	mfaSecret?: string;
 	emailVerified: boolean;
-	emailVerificationToken: string;
-	emailVerificationExpire: Date;
+	emailVerificationToken?: string;
+	emailVerificationExpire?: Date;
 	active: boolean;
 }
 
 interface IUserDocument extends IUser, Document {
 	matchPassword(enteredPassword: string): Promise<boolean>;
+	signAccessToken(): string;
+
+	signRefreshToken(): string;
 }
 
 const userSchema = new Schema<IUserDocument>(
@@ -49,6 +55,12 @@ const userSchema = new Schema<IUserDocument>(
 			trim: true,
 			lowercase: true,
 		},
+		phone: {
+			type: String,
+			required: [true, "Please provide your phone number"],
+			trim: true,
+			unique: true,
+		},
 		photo: {
 			type: String,
 			default: "default.jpg",
@@ -63,6 +75,7 @@ const userSchema = new Schema<IUserDocument>(
 			required: [true, "Please provide a password"],
 			select: false,
 			minlength: 8,
+			trim: true,
 		},
 		shippingAddress: {
 			street: { type: String },
@@ -86,11 +99,11 @@ const userSchema = new Schema<IUserDocument>(
 		mfaSecret: { type: String, select: false },
 		emailVerified: { type: Boolean, default: false },
 		emailVerificationToken: String,
-		emailVerificationExpire: Date,	
+		emailVerificationExpire: Date,
 		active: {
 			type: Boolean,
 			default: true,
-			select: false, 
+			select: false,
 		},
 	},
 	{ timestamps: true }
@@ -98,16 +111,26 @@ const userSchema = new Schema<IUserDocument>(
 
 // ? Schema Middleware
 userSchema.pre("save", async function (next) {
-	if (this.isModified("password")) {
-		const salt = bcrypt.genSaltSync(10);
-		this.password = await bcrypt.hash(this.password, salt);
-	}
+	if (!this.isModified("password")) return next();
+	const salt = await bcrypt.genSalt(10);
+	this.password = await bcrypt.hash(this.password!, salt);
+	(this as any).passwordConfirm = undefined;
 	next();
 });
 
 // ? Schema Methods
 userSchema.methods.matchPassword = async function (enteredPassword: string) {
 	return await bcrypt.compare(enteredPassword, this.password);
+};
+
+userSchema.methods.signAccessToken = function () {
+	const payload = { id: this._id, email: this.email };
+	return jwt.sign(payload, config.jwtAccessTokenSecret, { expiresIn: "15m" });
+};
+
+userSchema.methods.signAccessToken = function () {
+	const payload = { id: this._id };
+	return jwt.sign(payload, config.jwtRefreshTokenSecret, { expiresIn: "7d" });
 };
 
 // ? Schema Virtuals
